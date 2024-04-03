965389e9d3d81207b8c86c8da0f1fd730d3b6c1e
jairo.ocaña <sf_17@softwareitfactory.com>
2024-04-03 08:50:42 +0200

    Ejemplos con Netonsoft y Octokit

--- TestingGithub/Program.cs
+++ TestingGithub/Program.cs
@@ -1,18 +1,73 @@
 ﻿using Octokit;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-static async Task Main(string[] args)
+class Program
 {
-    var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
-    github.Credentials = new Credentials("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"); // NOTA: Reemplaza "your-github-token" con tu token de GitHub
+    static async Task Main(string[] args)
+    {
+        await Octokit();
+        await Github();
+        
+    }
 
-    var branchName = "testing";
-    var owner = "jairocnsf"; // NOTA: Reemplaza "owner-name" con el nombre del propietario del repositorio
-    var repoName = "testing"; // NOTA: Reemplaza "repo-name" con el nombre del repositorio
+    static async Task Github()
+    {
+        try
+        {
+            var token = "token"; // NOTA: Tu token de GitHub
+            var owner = "jairocnsf"; // NOTA: Nnombre del propietario del repositorio
+            var repo = "TestingGithub"; // NOTA: Nombre del repositorio
+            var branchName = "production";
 
-    // Obtén la referencia de la rama 'master'
-    var masterReference = await github.Git.Reference.Get(owner, repoName, "heads/master");
-    var latestCommit = masterReference.Object.Sha;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "TestingGitHub");
+                client.DefaultRequestHeaders.Add("Authorization", $"token {token}");
 
-    // Crea una nueva referencia de rama apuntando al último commit
-    await github.Git.Reference.Create(owner, repoName, new NewReference($"refs/heads/{branchName}", latestCommit));
+                // Obtén la referencia de la rama 'master'
+                var masterRefResponse = await client.GetStringAsync($"https://api.github.com/repos/{owner}/{repo}/git/refs/heads/master");
+                var masterRefSha = JObject.Parse(masterRefResponse)["object"]["sha"].ToString();
+
+                // Crea una nueva referencia de rama apuntando al último commit
+                var newRefContent = $"{{\"ref\": \"refs/heads/{branchName}\", \"sha\": \"{masterRefSha}\"}}";
+                var newRefResponse = await client.PostAsync($"https://api.github.com/repos/{owner}/{repo}/git/refs", new StringContent(newRefContent, Encoding.UTF8, "application/json"));
+            }
+
+            Console.WriteLine("La rama ha sido creada");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Se produjo un error: {ex.Message}");
+        }
+    }
+
+    static async Task Octokit()
+    {
+        try
+        {
+            var github = new GitHubClient(new ProductHeaderValue("TestingGithub"));
+            github.Credentials = new Credentials("token"); // NOTA: Tu token de GitHub
+
+            var branchName = "issues";
+            var owner = "jairocnsf"; // NOTA: Nombre del propietario del repositorio
+            var repoName = "TestingGithub"; // NOTA: Nombre del repositorio
+
+            // Obtén la referencia de la rama 'master'
+            var masterReference = await github.Git.Reference.Get(owner, repoName, "heads/master");
+            var latestCommit = masterReference.Object.Sha;
+
+            // Crea una nueva referencia de rama apuntando al último commit
+            await github.Git.Reference.Create(owner, repoName, new NewReference($"refs/heads/{branchName}", latestCommit));
+
+            Console.WriteLine("La rama ha sido creada");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Se produjo un error: {ex.Message}");
+        }
+    }
 }
